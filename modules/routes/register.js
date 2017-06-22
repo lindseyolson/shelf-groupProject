fc2e5bfb23e17d0c5f862a4e47c129b11632a690
Carl Page <carlpage@Carls-MacBook-Air.local>
2017-06-22 14:39:52 -0500

    Got salted hash

--- modules/routes/register.js
+++ modules/routes/register.js
@@ -9,9 +9,35 @@ router.use(bodyParser.json());
 
 router.post('/', function( req, res ){
   console.log( 'in register post:', req.body );
-  res.sendStatus( 201 );
+  // use bcrypt to generate a salt
+  bcrypt.genSalt( 12, function( err, salt ){
+    if( err ){
+      console.log( 'salt err:', err );
+      res.sendStatus( 400 );
+    }
+    else{
+      console.log( 'salt:', salt );
+      bcrypt.hash( req.body.password, salt, function( err, hash){
+        if( err ){
+          console.log( 'hash err:', err );
+          res.sendStatus( 400 );
+        }
+        else{
+          console.log( 'hash:', hash);
+          // only save hashed password
+          var newUser = {
+            username: req.body.username,
+            password: hash
+          };
+          console.log( 'saving user:', newUser );
+          // save newUser to db
+          // exporting model from mongo
+          mongo( newUser ).save();
+          res.sendStatus( 201 );
+        }
+      });
+    }
+  });
 });
-
-
 
 module.exports = router;
